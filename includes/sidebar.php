ef68c2c28c8806a858b8bb1da372ffd46c73bc1f
SH0WR0V <sm.shahriar1230@gmail.com>
2023-11-24 18:32:40 +0600

    search

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,14 +3,16 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <div class="input-group">
-            <input type="text" class="form-control">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </span>
-        </div>
+        <form action="search.php" method="post">
+            <div class="input-group">
+                <input type="text" name="search" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="submit" name="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
         <!-- /.input-group -->
     </div>
 
@@ -20,18 +22,28 @@
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
+
+                    <?php
+                    $query = "select * from categories";
+                    $result = mysqli_query($connection, $query);
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+                    ?>
+
+                    <!-- <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
                 <ul class="list-unstyled">
                     <li><a href="#">Category Name</a>
                     </li>
@@ -42,16 +54,13 @@
                     <li><a href="#">Category Name</a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include 'widget.php'; ?>
 
 </div>
