7da6e932876d761b7234713160570388f866f43c
SH0WR0V <sm.shahriar1230@gmail.com>
2023-11-30 01:46:49 +0600

    delete categories

--- admin/categories.php
+++ admin/categories.php
@@ -26,8 +26,27 @@ include "includes/header.php";
                             Welcome to Admin
                             <small>Author</small>
                         </h1>
+
+
+
                         <div class="col-xs-6">
-                            <form action="">
+
+                            <?php
+                            if (isset($_POST['submit'])) {
+                                $cat_title = $_POST['cat_title'];
+                                if ($cat_title == "" || empty($cat_title)) {
+                                    echo "<h4>* this field should not be empty</h4>";
+                                } else {
+                                    $query = "INSERT INTO categories(cat_title) value('$cat_title')";
+                                    $create_query = mysqli_query($connection, $query);
+                                    if (!$create_query) {
+                                        die('query failed' . mysqli_error($connection));
+                                    }
+                                }
+                            }
+                            ?>
+
+                            <form action="" method="post">
                                 <div class="form-group">
                                     <label for="cat_title">Add Category</label>
                                     <input type="text" name="cat_title" class="form-control">
@@ -57,8 +76,17 @@ include "includes/header.php";
                                         echo "<tr>";
                                         echo "<td>{$cat_id}</td>";
                                         echo "<td>{$cat_title}</td>";
+                                        echo "<td><a href = 'categories.php?delete={$cat_id}'>Delete</a></td>";
                                         echo "</tr>";
                                     }
+
+                                    if (isset($_GET['delete'])) {
+                                        $get_cat_id = $_GET['delete'];
+                                        $query = "DELETE FROM categories WHERE cat_id = $get_cat_id";
+                                        mysqli_query($connection, $query);
+                                        header("Location: categories.php");
+                                    }
+
                                     ?>
 
                                 </tbody>
